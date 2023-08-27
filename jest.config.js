7767853a3f5340c0f89e1a522c5c07c3266b3703
Azizbek Nomanoff <im.nomanoff@gmail.com>
2023-08-27 21:16:03 +0900

    ci: configure Jest, ESLint, and Next.js version in CI pipeline

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,18 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/$1'
-  }
+const nextJest = require('next-jest');
+
+const createJestConfig = nextJest({
+  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+  dir: './'
+});
+
+// Add any custom config to be passed to Jest
+/** @type {import('jest').Config} */
+const config = {
+  // Add more setup options before each test is run
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testEnvironment: 'jest-environment-jsdom',
+  preset: 'ts-jest'
 };
+
+// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+module.exports = createJestConfig(config);
