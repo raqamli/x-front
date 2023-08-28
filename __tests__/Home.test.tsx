7cc0117883a9d8314395bc9c6ffef5516a882ac0
Azizbek Nomanoff <im.nomanoff@gmail.com>
2023-08-28 12:28:57 +0900

    test: added some random tests for home screen

--- __tests__/Home.test.tsx
+++ __tests__/Home.test.tsx
@@ -3,9 +3,23 @@ import Home from '@/app/page';
 
 // random test just to make sure it works
 describe('Home', () => {
-    it('should have a Docs text', () => {
+    it('should have a "Docs" text', () => {
         render(<Home />);
         const myElement = screen.getByText(/Docs/i); // ACT
+        expect(myElement).toBeInTheDocument(); // ASSERT
+    });
+
+    it('should contain the text "information"', () => {
+        render(<Home />);
+        const myElement = screen.getByText(/info/i); // ACT
+        expect(myElement).toBeInTheDocument(); // ASSERT
+    });
+
+    it('should have a heading', () => {
+        render(<Home />);
+        const myElement = screen.getByRole('heading', {
+            name: "Learn"
+        }); // ACT
         expect(myElement).toBeInTheDocument(); // ASSERT
     });
 });
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -60,7 +60,7 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2>
-            Learn <span>-&gt;</span>
+            Learn
           </h2>
           <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
         </a>
