a7baa4625dd1070ffefc2dbec37b9703045f347b
Azizbek Nomanoff <im.nomanoff@gmail.com>
2023-08-28 11:46:26 +0900

    fix: fixed the test error on Home.tsx

--- __tests__/Home.test.tsx
+++ __tests__/Home.test.tsx
@@ -3,12 +3,9 @@ import Home from '@/app/page';
 
 // random test just to make sure it works
 describe('Home', () => {
-
-it('should have a Docs text', () => {
-    render(<Home />);
-
-    const myElement = screen.getByText(/Docs/i); // ACT
-    expect(myElement).toBeInTheDocument(); // ASSERT
-});
-
+    it('should have a Docs text', () => {
+        render(<Home />);
+        const myElement = screen.getByText(/Docs/i); // ACT
+        expect(myElement).toBeInTheDocument(); // ASSERT
+    });
 });
