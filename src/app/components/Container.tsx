fd304622c78eb3d437dc121513466afcbf41f205
AbdulxaqDev <abdulkhak8tursunov@gmail.com>
2023-08-30 12:53:15 +0300

    modify: container component

--- src/app/components/Container.tsx
+++ src/app/components/Container.tsx
@@ -1,15 +1,7 @@
 import { calcRem } from '@/styles/theme';
 import styled from 'styled-components';
 
-const Div = styled.div<{ $vertical?: boolean, $minWidth?: number, $minHeight?: number, $alignItems?: string }>`
-  display: flex;
-  flex-direction: ${props => props.$vertical ? "column" : "row"};
-  justify-content: center;
-  align-items: ${({ $alignItems }) => $alignItems ? $alignItems : "center"} ;
-  min-width: ${({ $minWidth }) => $minWidth ? calcRem($minWidth) : "100vw"} ;
-  min-height: ${({ $minHeight }) => $minHeight ? calcRem($minHeight) : "100vh"} ;
-  border: red solid 1px;
-`;
+
 
 type ContainerPropsType = {
     children: React.ReactNode;
@@ -17,20 +9,37 @@ type ContainerPropsType = {
     minWidth?: number;
     minHeight?: number;
     alignItems?: string;
+    justifyContent?: string;
 };
+
+const Div = styled.div<{ $vertical?: boolean, $minWidth?: number, $minHeight?: number, $alignItems?: string, $justifyContent?: string }>`
+  display: flex;
+  position: relative;
+  flex-direction: ${props => props.$vertical ? "column" : "row"};
+  justify-content: ${({ $justifyContent }) => $justifyContent ? $justifyContent : "center"};
+  align-items: ${({ $alignItems }) => $alignItems ? $alignItems : "center"} ;
+  min-width: ${({ $minWidth }) => $minWidth ? calcRem($minWidth) : "100vw"};
+  min-height: ${({ $minHeight }) => $minHeight ? calcRem($minHeight) : "100vh"};
+  max-height: 100vh;
+  overflow-y: hidden;
+  overflow-x: hidden;
+`;
 
 export function Container({
     children,
     vertical,
     minWidth,
     minHeight,
-    alignItems }: ContainerPropsType) {
+    alignItems,
+    justifyContent,
+}: ContainerPropsType) {
     return (
         <Div
             $vertical={vertical}
             $minWidth={minWidth}
             $minHeight={minHeight}
             $alignItems={alignItems}
+            $justifyContent={justifyContent}
         >
             {children}
         </Div>);
